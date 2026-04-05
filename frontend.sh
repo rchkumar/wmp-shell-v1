180ade7d63290441e8d63a2e35905fd5a958d63f
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2026-04-05 15:23:26 +0800

    Moved copy nginx conf command to another line

--- frontend.sh
+++ frontend.sh
@@ -2,6 +2,8 @@ dnf module disable nginx -y
 dnf module enable nginx:1.26 -y
 dnf install -y nginx
 
+cp nginx.conf /etc/nginx/nginx.conf
+
 systemctl enable nginx
 systemctl start nginx
 
@@ -24,7 +26,7 @@ npm run build
 rm -rf /usr/share/nginx/html/*
 cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
 
-cp nginx.conf /etc/nginx/nginx.conf
+
 
 
 
