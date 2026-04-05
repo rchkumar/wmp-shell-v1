03a15241b1ad29265d7050f8e49e614c12266084
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2026-04-05 17:45:15 +0800

    Updated hba file and replace values

--- postgressq.sh
+++ postgressq.sh
@@ -3,3 +3,10 @@ dnf -qy module disable postgresql
 dnf install -y postgresql16-server postgresql16
 /usr/pgsql-16/bin/postgresql-16-setup initdb
 sed -i "s/^#\?listen_addresses.*/listen_addresses = '*'/" /var/lib/pgsql/16/data/postgresql.conf
+
+FILE="/var/lib/pgsql/16/data/pg_hba.conf"
+
+sed -i "s/^local[[:space:]]\+all[[:space:]]\+all[[:space:]]\+peer/local   all             all                                     trust/" "$FILE"
+
+grep -q "^host[[:space:]]\+all[[:space:]]\+all[[:space:]]\+0.0.0.0/0[[:space:]]\+scram-sha-256" "$FILE" || \
+echo "host    all    all    0.0.0.0/0    scram-sha-256" >> "$FILE"
