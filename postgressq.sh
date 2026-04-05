d4915d9590e0706e597674f72213682c10226a16
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2026-04-05 17:32:54 +0800

    added SED line

--- postgressq.sh
+++ postgressq.sh
@@ -2,3 +2,4 @@ dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_6
 dnf -qy module disable postgresql
 dnf install -y postgresql16-server postgresql16
 /usr/pgsql-16/bin/postgresql-16-setup initdb
+sed -i "s/^#\?listen_addresses.*/listen_addresses = '*'/" /var/lib/pgsql/16/data/postgresql.conf
